1a02f9218e1f5c544e2858d80c4f1a800750ff8c
Dominic Dambrogia <dominic@gigasavvy.com>
2017-10-28 23:44:56 -0700

    added mysql to Proof of Concept

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Hello Docker!";
+$host = '127.0.0.1';
+$user = 'root';
+$pass = 'docker';
+
+$conn = new mysqli($host, $user, $pass);
+
+$sql = 'show databases';
+$results = $conn->query($sql);
+
+?>
+
+<h1>Hello Docker!</h1>
+
+<ul>
+    <?php while ($row = $results->fetch_assoc()) : ?>
+        <li><?= $row['Database'] ?></li>
+    <?php endwhile ?>
+</ul>
